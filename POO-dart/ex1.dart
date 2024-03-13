b05a478c706cc2c0d2b0630ca9437aed0c7db5ec
paol4zchs <133895456+paol4zchs@users.noreply.github.com>
2024-03-13 12:40:33 -0300

    Add files via upload

---
+++ POO-dart/ex1.dart
@@ -0,0 +1,67 @@
+
+class Pessoa {
+  String? _nome;
+  int? _idade;
+  String? _endereco ;
+  String? _profissao;
+  int? _cpf;
+  int? _rg;
+
+  String get nome => _nome!;
+
+  set nome(String value){
+    _nome = value;
+  }
+
+  int get idade => _idade!;
+
+  set idade(int value){
+    _idade = value;
+  }
+
+   String get endereco => _endereco!;
+
+  set endereco(String value){
+    _endereco = value;
+  }
+
+  String get profissao => _profissao!;
+
+  set profissao(String value){
+    _profissao = value;
+  }
+
+  int get cpf => _cpf!;
+
+  set cpf(int value){
+    _cpf = value;
+  }
+
+  int get rg => _rg!;
+
+  set rg(int value){
+    _rg = value;
+  }
+
+
+}
+
+void main(){
+
+  Pessoa pessoa = new Pessoa();
+
+  pessoa.nome= "Paola";
+  pessoa.idade= 17;
+  pessoa.endereco = "Votorantim";
+  pessoa.profissao= "??" ;
+  pessoa._cpf = 47216068858;
+  pessoa.rg= 1234567890;
+
+  print("Nome: ${pessoa.nome}");
+  print("Idade: ${pessoa.idade}");
+  print("Endereço: ${pessoa.endereco}");
+   print("Profissão: ${pessoa.profissao}");
+    print("Cpf: ${pessoa.cpf}");
+     print("Rg: ${pessoa.rg}");
+}
+
---
+++ POO-dart/ex3.dart
@@ -0,0 +1,41 @@
+class Aviao{
+    String? _cor, _modelo, _marca, _ano;
+
+    String get marca => _marca!;
+
+  set marca(String value){
+    _marca = value;
+  }
+
+  String get modelo => _modelo!;
+
+  set modelo(String value){
+    _modelo = value;
+  }
+
+  String get ano => _ano!;
+
+  set ano(String value){
+    _ano = value;
+  }
+
+  String get cor => _cor!;
+
+  set cor(String value){
+    _cor = value;
+  }
+}
+
+void main(){
+  Aviao aviao = new Aviao();
+
+  aviao.marca="AIRBUS";
+  aviao.ano="	1987–2014";
+  aviao.modelo="A320";
+  aviao.cor="Branco";
+
+   print("Marca: ${aviao.marca}");
+   print("Ano: ${aviao.ano}");
+   print("Modelo: ${aviao.modelo}");
+   print("Cor: ${aviao.cor}");
+}
